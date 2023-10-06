eda717af2e7d775359cd5f4ac1eb54f8d071cbae
abbiewood <139762394+abbiewood@users.noreply.github.com>
2023-10-07 12:42:03 +1300

    Adding in sun features

--- music_starter.js
+++ music_starter.js
@@ -13,7 +13,7 @@ strokeWeight (3)
 
 let sizeOfCanvas = 800;
 
-
+//gradient code
 for (let i = 0; i < sizeOfCanvas; i++ ){
 let gradientAmount = map (i, 0, sizeOfCanvas, 0, 1)
 let strokeColor = lerpColor(blueColor, pinkColor, gradientAmount)
@@ -22,16 +22,53 @@ stroke (strokeColor)
 line (0, i, width, i)
 }
 
-//sun
-fill ("#ffd67d")
-ellipse(1000, 800, 300);
-//create a map here to get it to slowly rise with a certain point in the song to have rays from it.
+//vocal sun
+let sunYStart = height/0;
+let sunYEnd = height/8;
+let whenSingingStart = 25.5 //put in here when the first line is sung
 
+let sunYMap = map(song.currentTime(), 0, song.duration(),800, 0)
+strokeWeight (0)
+
+drawSun(1000,sunYMap, vocal)
+//circle(1000, 800 + vocal, 300 + vocal )
 
 //sea
-fill ("#B9CEC3")
+fill ("#B9CEC3") // blue
 rect(0, 800, width+2, 600);
 //add in image with extra texture and movement.
 
+}
+
+function drawSun(x, y, V){
+  push()
+
+  ellipseMode(CENTER);
+  translate(x,y)
+
+  let rayColor = color(255,0,0, 100) // last variable is opacity 
+
+  strokeWeight(10)
+
+  stroke(rayColor)
+
+  // you can figure out how to use a for loop here! Ask us for help if needed 
+  rotate(360/8)
+  line(300 + V, 0, -(300 + V), 0)
+
+  rotate(360/8)
+  line(300 + V, 0, -(300 + V), 0)
+
+  rotate(360/8)
+  line(300 + V, 0, -(300 + V), 0)
+
+  rotate(360/8)
+  line(300 + V, 0, -(300 + V), 0)
+
+  fill ("#ffd67d")
+  noStroke()
+  circle(0,0, 300 + V );
+
+  pop()
 
 }
